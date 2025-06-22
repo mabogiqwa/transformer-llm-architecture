ca73eb838634186b95eec82e17f746e75dd9de3c
Mabo Giqwa <mabogiqwa1@gmail.com>
2025-06-22 15:56:21 +0200

    Multi-class self-attention class definition

--- phase1_tiny_llm/attention.py
+++ phase1_tiny_llm/attention.py
@@ -4,4 +4,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class MultiHeadSelfAttention(nn.Module):
-    
+    def __init__(self, d_model, n_heads):
+        super().__init__()
+        assert d_model % n_heads == 0, "d_model must be divisible by n_heads"
+
+        self.d_model = d_model
+        self.n_heads = n_heads
+        self.head_dim = d_model // n_heads
+
+        #Linear projections for Q,K,V
+        self.q_proj = nn.Linear(d_model, d_model)
+        self.k_proj = nn.Linear(d_model, d_model)
+        self.v_proj = nn.Linear(d_model, d_model)
+
+        #Final linear projection
+        self.out_proj = nn.Linear(d_model, d_model)
